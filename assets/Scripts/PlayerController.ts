ddbcbae89588584d9d7f1b9dc6f8bacd5c2fd6ff
DungLa <leanhdung3105@gmail.com>
2026-03-16 14:09:17 +0700

    feat: game over

--- assets/Scripts/PlayerController.ts
+++ assets/Scripts/PlayerController.ts
@@ -1,26 +1,30 @@
-import { _decorator, Component, input, Input, EventTouch, Prefab, instantiate } from 'cc';
+import { _decorator, Component, input, Input, EventTouch, Prefab, instantiate, Collider2D, Contact2DType, IPhysics2DContact, director,PhysicsSystem2D } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('PlayerController')
 export class PlayerController extends Component {
 
-    // Khai báo một lỗ cắm để lát nữa kéo thả khuôn đúc (Prefab) vào
     @property({type: Prefab})
     bulletPrefab: Prefab | null = null; 
 
     @property
-    shootInterval: number = 0.2; // Tốc độ xả đạn: 0.2 giây / viên
+    shootInterval: number = 0.2; 
 
     start() {
         input.on(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
-        
-        // Dùng hàm schedule của Cocos để gọi hàm bắn đạn lặp đi lặp lại
         this.schedule(this.shoot, this.shootInterval);
+
+        // 1. Lấy component Collider2D của phi thuyền
+        let collider = this.getComponent(Collider2D);
+        if (collider) {
+            // 2. Lắng nghe sự kiện va chạm
+            collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+        }
     }
 
     onDestroy() {
         input.off(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
-        this.unschedule(this.shoot); // Hủy bắn khi tàu bị nổ
+        this.unschedule(this.shoot); 
     }
 
     onTouchMove(event: EventTouch) {
@@ -32,17 +36,30 @@ export class PlayerController extends Component {
     }
 
     shoot() {
-        if (!this.bulletPrefab) return; // Tránh lỗi nếu bạn quên kéo Prefab vào
+        if (!this.bulletPrefab) return; 
 
-        // 1. Dùng lệnh instantiate để đúc ra 1 viên đạn từ khuôn
         let bullet = instantiate(this.bulletPrefab);
-        
-        // 2. Thêm viên đạn vào Scene (Cho nó làm con của node Canvas)
         this.node.parent!.addChild(bullet); 
 
-        // 3. Đặt vị trí viên đạn xuất phát từ mũi tàu
         let playerPos = this.node.position;
-        // Cộng thêm 50px vào trục Y để đạn chui ra từ mũi phi thuyền chứ không phải từ giữa bụng
         bullet.setPosition(playerPos.x, playerPos.y + 50, playerPos.z);
+    }
+
+    onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        
+        if (otherCollider.node.getComponent('Enemy')) {
+            console.log("💥 TÀU BỊ TRÚNG THIÊN THẠCH! GAME OVER 💥");
+
+            this.scheduleOnce(() => {
+                if (this.node && this.node.isValid) {
+                    
+                    // Thêm dòng này để tắt hệ thống rơi của thiên thạch và đạn
+                    PhysicsSystem2D.instance.enable = false;
+
+                    director.pause();
+                    this.node.active = false; 
+                }
+            }, 0);
+        }
     }
 }
